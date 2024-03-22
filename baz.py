1f004c62f798ff229536b85278edbb43de230db8
Peter <peter.hofmann@stud.fra-uas.de>
2024-03-22 18:51:17 +0100

    Add functionality to retrieve sell prices for specific items from the Hypixel API

--- baz.py
+++ baz.py
@@ -6,18 +6,25 @@ l = "api.hypixel.net/skyblock/bazaar/products?key=8b47b741-8018-4ea2-a564-b01d53
 
 
 def get_products():
+    items = ['INK_SACK:3','COBBLESTONE','ENCHANTED_MELON_BLOCK','ENCHANTED_MELON','ENCHANTED_POTATO',
+         'ENCHANTED_POTATO','ENCHANTED_PUMPKIN','ENCHANTED_PUMPKIN','ENCHANTED_SUGAR_CANE','ENCHANTED_SUGAR_CANE',
+         'ENCHANTED_STRING','ENCHANTED_STRING']
     url = "https://api.hypixel.net/v2/skyblock/bazaar"
     response = requests.get(url)
     data = response.json()
-    with open('products.txt', 'w') as outfile:
-        json.dump(data, outfile)
+    for item in items:
+        print(item)
+        print(data['products'][item]['quick_status']['sellPrice'])
+
+    # Access the quick_status for the product "INK_SACK:3"
+    quick_status_ink_sack_3 = data['products']['INK_SACK:3']['quick_status']
+
+    # Print the sell price
+    print("Sell Price for INK_SACK:3:", quick_status_ink_sack_3['sellPrice'])
+        
     return data
 
-def get_moneyy():
-    with open('products.txt', 'r') as f:
-        for line in f:
-            if 'specific_word' in line:  # replace 'specific_word' with the word you're looking for
-                print(line)
+
 
 
 if __name__ == "__main__":
