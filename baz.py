c13b7b5bf0bea72bed327325a6374c9ae42e62f7
Peter <peter.hofmann@stud.fra-uas.de>
2024-03-23 16:54:42 +0100

    Calc Price basics

--- baz.py
+++ baz.py
@@ -7,7 +7,7 @@ l = "api.hypixel.net/skyblock/bazaar/products?key=8b47b741-8018-4ea2-a564-b01d53
 
 def get_products():
     items = ['COBBLESTONE','ENCHANTED_COBBLESTONE','ENCHANTED_MELON_BLOCK','ENCHANTED_MELON',
-         'ENCHANTED_POTATO','PUMPKIN','ENCHANTED_PUMPKIN','STRING','ENCHANTED_STRING',]
+         'ENCHANTED_POTATO','','ENCHANTED_PUMPKIN','STRING','ENCHANTED_STRING',]
     url = "https://api.hypixel.net/v2/skyblock/bazaar"
     response = requests.get(url)
     data = response.json()
@@ -20,16 +20,22 @@ def get_products():
     ##with open('data.json', 'w') as f:
         ##json.dump(data, f)
 
-    for item in items:
-        print(item)
-        sell_price = data['products'][item]['quick_status']['sellPrice']
-        rounded_sell_price = round(sell_price,1)
-        print(rounded_sell_price)
+    
         
     return data
+
+def calculate_price(data):
+    items160 = ['COBBLESTONE','PUMPKIN','STRING']
+    items8= []
+
+    for item in items160:
+        sell_price = data['products'][item]['quick_status']['sellPrice']
+        rounded_sell_price = round(sell_price,1)
+        price = rounded_sell_price * 160
+        print("Enchanted " + item+": " + str(price))
 
 
 
 
 if __name__ == "__main__":
-    get_products()
+    calculate_price(get_products())
