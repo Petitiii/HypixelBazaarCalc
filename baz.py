0b1b6f161cde4adb6dd2ca7a3b662dbcde0e0f24
Peter <peter.hofmann@stud.fra-uas.de>
2024-03-23 16:40:50 +0100

    Now deletes useless information from the json dump

--- baz.py
+++ baz.py
@@ -6,12 +6,20 @@ l = "api.hypixel.net/skyblock/bazaar/products?key=8b47b741-8018-4ea2-a564-b01d53
 
 
 def get_products():
-    items = ['INK_SACK:3','COBBLESTONE','ENCHANTED_MELON_BLOCK','ENCHANTED_MELON','ENCHANTED_POTATO',
-         'ENCHANTED_POTATO','ENCHANTED_PUMPKIN','ENCHANTED_PUMPKIN','ENCHANTED_SUGAR_CANE','ENCHANTED_SUGAR_CANE',
-         'ENCHANTED_STRING','ENCHANTED_STRING']
+    items = ['COBBLESTONE','ENCHANTED_COBBLESTONE','ENCHANTED_MELON_BLOCK','ENCHANTED_MELON',
+         'ENCHANTED_POTATO','PUMPKIN','ENCHANTED_PUMPKIN','STRING','ENCHANTED_STRING',]
     url = "https://api.hypixel.net/v2/skyblock/bazaar"
     response = requests.get(url)
     data = response.json()
+    for product in data['products']:
+        if 'sell_summary' in data['products'][product]:
+            del data['products'][product]['sell_summary']
+        if 'buy_summary' in data['products'][product]:
+            del data['products'][product]['buy_summary']
+
+    ##with open('data.json', 'w') as f:
+        ##json.dump(data, f)
+
     for item in items:
         print(item)
         sell_price = data['products'][item]['quick_status']['sellPrice']
