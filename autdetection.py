d8d344aa9720bbd8a29a352a6a6015516f792fb0
Peter <peter.hofmann@stud.fra-uas.de>
2024-07-22 11:47:37 +0200

    refactor: Improve OCR pattern matching and file writing in autdetection.py

--- autdetection.py
+++ autdetection.py
@@ -2,30 +2,31 @@ import cv2
 import numpy as np
 from PIL import ImageGrab
 import pytesseract
+import re
 
-# Konfigurieren Sie den Pfad zu Ihrem Tesseract-Executable
+
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'  # Pfad anpassen
 
-def monitor_area(region, target_word, output_file):
+def monitor_area(region, pattern, output_file):
     while True:
-        # Erfassen Sie einen Screenshot des spezifizierten Bereichs
-        print("Monitoring")
-        screen = ImageGrab.grab(bbox=region)  # x, y, Breite, Höhe
+        
+        screen = ImageGrab.grab(bbox=region)  
         screen_np = np.array(screen)
         screen_gray = cv2.cvtColor(screen_np, cv2.COLOR_BGR2GRAY)
 
-        # Verwenden Sie OCR, um Text aus dem Screenshot zu extrahieren
+        
         text = pytesseract.image_to_string(screen_gray)
-
-        # Prüfen Sie, ob das gesuchte Wort im Text ist
-        if target_word.lower() in text.lower():
-            # Speichern Sie den Text in einer Datei, wenn das Wort gefunden wird
+        match = re.search(pattern, text)
+        if match:
             with open(output_file, 'a') as file:
-                file.write(text + "\n")
-                file.write("="*50 + "\n")  # Separator für jede Erkennung
+                file.write(match.group() + "\n")  # Speicher den Satz
+                print(match.group())
 
-        # Sie können hier eine kurze Pause einlegen, wenn Sie möchten
+       
         # time.sleep(1)
 
-# Bereich [x_start, y_start, x_end, y_end], Zielwort, Ausgabedateiname
-monitor_area([333, 536,1270, 850], 'Pfad', 'detected_text.txt')
+# Regex-Muster für den gesuchten Satz, der jede Art von Produkt und Menge akzeptiert
+pattern = r"Your Buy Order Setup! \d+x \w+ for \d+(\.\d+)? Coins"
+
+# [x_start, y_start, x_end, y_end], Regex-Muster
+monitor_area([333, 536,1270, 850], pattern, 'detected_orders.txt')
